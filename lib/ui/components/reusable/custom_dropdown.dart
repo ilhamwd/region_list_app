50a3ad06c019bb9a5135e7bbaf705e3f6b275626
iilhamwd <iilhamwd@gmail.com>
2023-05-17 00:58:33 +0700

    fix/dropdown customization for web

--- lib/ui/components/reusable/custom_dropdown.dart
+++ lib/ui/components/reusable/custom_dropdown.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CustomDropdown<T> extends StatelessWidget {
@@ -6,26 +7,40 @@ class CustomDropdown<T> extends StatelessWidget {
       required this.items,
       required this.onChanged,
       this.value,
-      this.hint});
+      this.hint,
+      this.isExpanded,
+      this.isDense,
+      this.padding,
+      this.borderRadius});
 
   final List<DropdownMenuItem<T>> items;
   final void Function(T? value) onChanged;
   final T? value;
   final String? hint;
+  final bool? isExpanded, isDense;
+  final EdgeInsetsGeometry? padding;
+  final BorderRadius? borderRadius;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+      padding:
+          padding ?? const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+      decoration: kIsWeb
+          ? BoxDecoration(
+              border: Border.all(color: const Color(0xFFDDDDDD)),
+              borderRadius: borderRadius ?? BorderRadius.circular(10))
+          : BoxDecoration(
+              color: Colors.white,
+              borderRadius: borderRadius ?? BorderRadius.circular(10)),
       child: DropdownButton<T>(
-        isExpanded: true,
+        isExpanded: isExpanded ?? true,
         items: items,
         onChanged: onChanged,
+        isDense: isDense ?? false,
         value: value,
         underline: const SizedBox(),
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: borderRadius ?? BorderRadius.circular(10),
         hint: hint == null ? null : Text(hint!),
       ),
     );
